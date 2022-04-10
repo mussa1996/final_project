69df16ea388a3d89cbd15352d34627f46f10631b
Niyodusenga Mussa <niyodusengamussa01@gmail.com>
2022-04-10 19:13:28 +0200

    added configuration

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import connectDb from './src/database/dbconnection';
+import cors from "cors";
+// import routes from './src/routers/index';
+
+const app = express();
+connectDb(); 
+let corsOptions ={
+  origin:'*', 
+  credentials:true,     
+  optionSuccessStatus:200
+}
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use('/api/v1/smart/');
+app.get("/",(req, res) => {
+  res.send("Smart City Project");
+});  
+const PORT = process.env.PORT || 4500;
+
+app.listen(PORT, () => {
+  console.log("server up and running on " + PORT);
+});
